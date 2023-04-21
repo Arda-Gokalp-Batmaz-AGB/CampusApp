ad27ee439515533609b73ed895e2595849e24724
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-21 23:12:46 +0300

    (MINOR) 2

--- Client/CampusLink/app/src/main/java/com/arda/campuslink/MainActivity.kt
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android Deneme1234faaa")
+                    Greeting("Android Deneme1234faaaaf")
                 }
             }
         }
