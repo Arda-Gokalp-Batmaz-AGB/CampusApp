197597e2c156b2b4dabe1c36190ceb0d3a1274a0
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-22 14:01:43 +0300

    add main app class

---
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/App.kt
@@ -0,0 +1,21 @@
+package com.arda.mainapp
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    companion object {
+        lateinit var context: Context
+        fun getAppContext(): Context? {
+            return context
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = getApplicationContext()
+    }
+
+}
