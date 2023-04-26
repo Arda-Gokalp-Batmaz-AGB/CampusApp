9e5a0c5993fbd4e13a73803d55612adb69fa3868
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-26 20:26:12 +0300

    add publish viewmodel and state

--- Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/publishscreen/PublishUiState.kt
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/publishscreen/PublishUiState.kt
@@ -1,4 +1,13 @@
 package com.arda.campuslink.ui.screens.publishscreen
 
-class PublishUiState {
-}
+import android.graphics.Bitmap
+import com.arda.campuslink.domain.model.FeedPost
+import com.arda.campuslink.domain.model.User
+import com.arda.mainapp.auth.Resource
+
+data class PublishUiState(
+    val postCreateFlow: Resource<FeedPost>? = null,
+    val currentMinimizedUser: User? = null,
+    val description : String = "",
+    val image : Bitmap? = null,
+)
--- Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/publishscreen/PublishViewModel.kt
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/publishscreen/PublishViewModel.kt
@@ -1,4 +1,58 @@
 package com.arda.campuslink.ui.screens.publishscreen
 
-class PublishViewModel {
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arda.campuslink.domain.usecase.CreatePostUseCase
+import com.arda.campuslink.domain.usecase.LoggedUserUseCase
+import com.arda.campuslink.ui.screens.leftbarpopup.LeftBarPopUpUiState
+import com.arda.campuslink.util.DebugTags
+import com.arda.mainapp.auth.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PublishViewModel @Inject constructor(
+    private val createPostUseCase: CreatePostUseCase,
+    private val loggedUserUseCase: LoggedUserUseCase
+) : ViewModel(), LifecycleObserver {
+    private val _uiState = MutableStateFlow(PublishUiState())
+    val uiState: StateFlow<PublishUiState> = _uiState.asStateFlow()
+
+    init {
+        _uiState.update {
+            it.copy(currentMinimizedUser = loggedUserUseCase.getMinProfileOfCurrentUser())
+        }
+    }
+
+    fun createPost() = viewModelScope.launch {
+        _uiState.update {
+            it.copy(postCreateFlow = Resource.Loading)
+        }
+        val result =
+            createPostUseCase.createNewPost(_uiState.value.description, _uiState.value.image)
+        _uiState.update {
+            it.copy(postCreateFlow = result)
+        }
+    }
+
+    fun updateDescription(textValue: String) {
+        _uiState.update {
+            it.copy(description = textValue)
+        }
+        Log.v(DebugTags.UITag.tag, "Current Description = ${_uiState.value.description}")
+    }
+
+    fun updateImage(image: Bitmap) {
+        _uiState.update {
+            it.copy(image = image)
+        }
+    }
 }
