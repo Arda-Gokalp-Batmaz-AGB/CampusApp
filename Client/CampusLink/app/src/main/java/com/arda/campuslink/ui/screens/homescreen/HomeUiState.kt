677ef781cd2cd2a861a0ba332ee00dacfdb18efe
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-04-26 22:15:04 +0300

    add functions and states for fetching new posts

---
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/homescreen/HomeUiState.kt
@@ -0,0 +1,11 @@
+package com.arda.campuslink.ui.screens.homescreen
+
+import android.graphics.Bitmap
+import com.arda.campuslink.domain.model.FeedPost
+import com.arda.campuslink.domain.model.User
+import com.arda.mainapp.auth.Resource
+
+data class HomeUiState(
+    val feedFlow: Resource<ArrayList<FeedPost>>? = null,
+    val currentFeed: ArrayList<FeedPost> = arrayListOf(),
+)
---
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/ui/screens/homescreen/HomeViewModel.kt
@@ -0,0 +1,52 @@
+package com.arda.campuslink.ui.screens.homescreen
+
+import android.util.Log
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arda.campuslink.domain.model.FeedPost
+import com.arda.campuslink.domain.usecase.LoggedUserUseCase
+import com.arda.campuslink.domain.usecase.UserPostFeedUseCase
+import com.arda.campuslink.ui.screens.publishscreen.PublishUiState
+import com.arda.campuslink.util.DebugTags
+import com.arda.mainapp.auth.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val loggedUserUseCase: LoggedUserUseCase,
+    private val userPostFeedUseCase: UserPostFeedUseCase
+) : ViewModel(), LifecycleObserver
+{
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
+
+    init {
+        fetchNewPosts()
+    }
+    fun fetchNewPosts() = viewModelScope.launch {
+        _uiState.update {
+            it.copy(feedFlow = Resource.Loading)
+        }
+        val result = userPostFeedUseCase.fetchNewPostsToFeed(loggedUserUseCase.getMinProfileOfCurrentUser().UID)
+        _uiState.update {
+            it.copy(feedFlow = result)
+        }
+    }
+    fun updateCurrentFeed(newFeed : ArrayList<FeedPost>)
+    {
+        _uiState.update {
+            val temp = arrayListOf<FeedPost>()
+            temp.addAll(it.currentFeed)
+            temp.addAll(newFeed)
+            it.copy(currentFeed = temp)
+        }
+        Log.v(DebugTags.UITag.tag,"Current updated feed = ${_uiState.value.currentFeed}")
+    }
+}
