ac1cab706b8e82a274c5df2aa0eddc680af5d546
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2023-05-07 12:33:36 +0300

    add a functionality tto add newly added post of the user to the feed

--- Client/CampusLink/app/src/main/java/com/arda/campuslink/data/repository/PostRepositoryImpl.kt
+++ Client/CampusLink/app/src/main/java/com/arda/campuslink/data/repository/PostRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.arda.campuslink.data.repository
 
+import com.arda.campuslink.R
 import com.arda.campuslink.data.DUMMY_FEED_DATA
+import com.arda.campuslink.data.dummyUserData
 import com.arda.campuslink.domain.model.FeedPost
 import com.arda.campuslink.domain.model.NewPost
 import com.arda.campuslink.domain.repository.AuthRepository
@@ -12,8 +14,10 @@ import com.google.firebase.functions.FirebaseFunctions
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.collections.ArrayList
 
 @Singleton
 class PostRepositoryImpl @Inject constructor(
@@ -27,10 +31,26 @@ class PostRepositoryImpl @Inject constructor(
             dispatcher
         )
         {
+//            val postID: UUID = UUID.randomUUID()
             return@withContext try {
                 //val result = auth.signInWithEmailAndPassword(email, password).await()
                     //Return edilen postu FeedPost Yap öyle al
-                Resource.Sucess(DUMMY_FEED_DATA[2]!!)
+                        // Postu databaseye ekle sonra databaseden dönen değeri feedpost yap geri dönder
+                            //hashtag bulma arkaplan
+                val feedPost = FeedPost(
+                    postId = "qweqwewq134314",
+                    user = newPost.user,
+                    description = newPost.description,
+                    image = null,
+                    likes = 0,
+                    comments = 0,
+                    shares = 0,
+                    views = 0,
+                    timestamp = newPost.timestamp,
+                    hashTags = arrayOf()
+                )
+                userPostFeed.add(feedPost)
+                Resource.Sucess(feedPost)
             } catch (e: Exception) {
                 e.printStackTrace()
                 Resource.Failure<Exception>(e)
@@ -65,10 +85,10 @@ class PostRepositoryImpl @Inject constructor(
     dispatcher
     ) {
         val posts = arrayListOf<FeedPost>()
-        userPostFeed.add(DUMMY_FEED_DATA[0])
-        userPostFeed.add(DUMMY_FEED_DATA[1])
         posts.add(DUMMY_FEED_DATA[0])
         posts.add(DUMMY_FEED_DATA[1])
+        posts.addAll(userPostFeed)
+        userPostFeed.clear()
         return@withContext try {
             Resource.Sucess(posts!!)
         } catch (e: Exception) {
